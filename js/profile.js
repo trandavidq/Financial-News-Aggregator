77e00530d58384f3a9bb01d8f806dfc5f3b46a21
David Tran <trandavidq@gmail.com>
2021-05-31 13:16:21 -0400

    Added pseudoclass CSS to login,signup,profile pages. Implemented JS to profile page. Fixed navbar links

---
+++ js/profile.js
@@ -0,0 +1,4 @@
+document.getElementById("submit_btn").addEventListener('click', ()=> {
+    console.log("aslkfj");
+    document.getElementById("success_submit").innerHTML = "Successfully submitted!";
+})
