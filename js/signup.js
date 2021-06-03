1e838e2f0c546f010cfc66852de4369675c562ad
David Tran <trandavidq@gmail.com>
2021-06-02 20:18:11 -0400

    Fix input validation

--- js/signup.js
+++ js/signup.js
@@ -3,7 +3,6 @@
 let username_form = document.getElementById("username");
 let password_form = document.getElementById("password");
 let toggle_password_checkbox = document.getElementById("toggle_password");
-username_form.focus();
 
 
 function checkUsername(){
@@ -11,7 +10,8 @@ function checkUsername(){
     let validUsername = username_form.value.length >5 && username_form.value.length >1 && !no_spaces;
 
     if(!validUsername){
-        document.getElementById("username_warning").innerHTML="Username must not contain any spaces, and must be greater than 5 characters long";
+        document.getElementById("username_warning").innerHTML="Username must not contain any spaces, and must be greater than 5 characters long!";
+        
     }
     else if (validUsername){
         document.getElementById("username_warning").innerHTML="";
@@ -20,10 +20,11 @@ function checkUsername(){
 
 function checkPassword(){
     let no_spaces = password_form.value.includes(" ");
-    let validPassword = password_form.value.length >5 &&  password_form.value.length>1 && !no_spaces;
+    let validPassword = password_form.value.length >5 && password_form.value.length>1 && !no_spaces;
 
     if(!validPassword){
         document.getElementById("password_warning").innerHTML="Password must not contain any spaces, and must be greater than 5 characters long";
+
     }
     else if (validPassword){
         document.getElementById("password_warning").innerHTML="";
@@ -40,6 +41,22 @@ function togglePassword(){
     }
 }
 
+function validInput(s){
+    if(s.length< 5 || s.includes(" ")){
+        return false;
+    }
+    return true;
+}
+
 username_form.addEventListener('input', checkUsername);
 password_form.addEventListener('input', checkPassword);
 toggle_password_checkbox.addEventListener('click',togglePassword);
+
+let submit_button = document.getElementById("submit_btn");
+submit_button.addEventListener('click',(event) => { 
+    //Change inner HTML
+    if(!validInput(username_form.value) || !validInput(password_form.value)){
+        event.preventDefault();
+    }
+});
+
--- login.php
+++ login.php
@@ -5,23 +5,25 @@
 
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="./styles/signup.css">
+	<link rel="stylesheet" href="./styles/login.css">
 	<!--Custom font for login text-->
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet"
 		integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
-	<title>Sign up</title>
+	
+	<title>Log in</title>
 </head>
 
-<body style="background-color: E7E1D7;">
+<body style="background-color: #E7E1D7;">
+
 	<h1 style="margin-top: 2%; text-align: center;">Financial News Aggregator</h1>
-	<h2 id="welcome_text">Signup here</h2>
+	<h2 id="welcome_text">Login here</h2>
 
 
 	<div id="login_form">
-		<!--Change the action to a post request later-->
-		<form action="login.html">
+		<!-- Change the action to a post request later -->
+		<form action="<?php echo $_SERVER["PHP_SELF"]; ?>">
 			<div class="form-group">
 				<label for="username">Username:</label>
 				<input type="text" id="username" name="username" class="form-control" style="width: 500px;"
@@ -40,21 +42,17 @@
 				<input type="checkbox" id="toggle_password">Show Password
 			</div>
 			<br><br>
-			<div class="form-group">
-				<label for="confirm_password">Confirm password:</label>
-				<input type="password" id="confirm_password" name="confirm_password" style="width: 500px;"
-					class="form-control">
-			</div>
-			<br><br>
-			<button type="submit" value="signup" class="btn btn-primary">Sign up</button>
+			<button type="submit" value="Login" class="btn btn-primary" id="submit_btn">Log in</button>
 		</form>
 
 
-		<a href="login.html" class="highlight">Already registered? Log in here</a>
-
-
+		<a href="signup.php" class="highlight">Haven't registered yet? Sign up here</a>
+		<br><br>
 	</div>
-	<script src="./js/signup.js"></script>
+
+	
+	
+	<script src="./js/login.js"></script>
 </body>
 
 </html>
